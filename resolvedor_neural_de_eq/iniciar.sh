f5fba496e9ef20a114bdf223388908c270d9d782
Lucas Dallamico <lucas.dallamico@hotmail.com>
2020-07-11 17:52:26 -0300

    Configurando a pagina para redirecionar para a minha hospedada no heroku

--- resolvedor_neural_de_eq/iniciar.sh
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/bin/bash
-echo ligando os agentes ...
-gnome-terminal -- python3 generatorRNA.py
-gnome-terminal -- python3 resolvedor.py
-
--- resolvedor_neural_de_eq/readme.txt
+++
@@ -1,3 +0,0 @@
-Para executar os agentes, basta abrir o terminal e digitar:
-./iniciar.sh
--> O script faz tudo sozinho ( para o bem da sua saude)
